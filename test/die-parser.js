912376d6400c7238e151bb75d85f91546cdb1c55
Ash Wilson <smashwilson@gmail.com>
2016-03-12 14:31:16 -0500

    Whitespace

--- test/die-parser.js
+++ test/die-parser.js
@@ -14,9 +14,12 @@ describe('die parser', () => {
   it('parses a die roll', parsesAs('1d20', '(d (i 1) (i 20))'));
   it('defaults die count to 1', parsesAs('d20', '(d (i 1) (i 20))'));
 
-  it('parses addition', parsesAs('1d10+2d6+7',
+  it('parses addition', parsesAs('1d10 + 2d6 + 7',
     '(+ (d (i 1) (i 10)) (+ (d (i 2) (i 6)) (i 7)))'));
 
-  it('parses subtraction', parsesAs('1-3+3d4',
+  it('parses subtraction', parsesAs('1 - 3 + 3d4',
     '(- (i 1) (+ (i 3) (d (i 3) (i 4))))'));
+
+  it('parses parens', parsesAs('(1 + 2)d(10 - 5)',
+    '(d (+ (i 1) (i 2)) (- (i 10) (i 5)))'));
 });
