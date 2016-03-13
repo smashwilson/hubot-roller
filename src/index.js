bbd5ba5bc5012be770c7161f7c9651c9c014e2ab
Ash Wilson <smashwilson@gmail.com>
2016-03-13 12:53:52 -0400

    Hubot command.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-'use index';
+'use strict';
+
+const dieParser = require('./die-parser');
+const report = require('./ast').report;
 
 module.exports = (robot) => {
   robot.respond(/roll\s*([^]+)/i, (msg) => {
-    //
+    let die = dieParser(msg.match[1]);
+    msg.reply(report(die));
   });
 };
