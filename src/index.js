a8a1b46acebb2b1c68cb8c342a56ff93eb23cb63
Ash Wilson <smashwilson@gmail.com>
2016-03-13 13:10:21 -0400

    Help text.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const dieParser = require('./die-parser');
 const report = require('./ast').report;
 
 module.exports = (robot) => {
+  // Help
+  robot.commands.push('hubot roll <expression> - Roll some dice and report the total. Examples: d20, 4d6 + 3, (1d6)d(2d4).');
+
   robot.respond(/roll\s*([^]+)/i, (msg) => {
     let die = dieParser.parse(msg.match[1]);
     msg.reply(report(die));
