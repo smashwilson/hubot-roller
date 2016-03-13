d971319df9c9240068d46b9c4bd7f474856db414
Ash Wilson <smashwilson@gmail.com>
2016-03-13 13:36:59 -0400

    Catch parsing errors.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ module.exports = (robot) => {
   robot.commands.push('hubot roll <expression> - Roll some dice and report the total. Examples: d20, 4d6 + 3, (1d6)d(2d4).');
 
   robot.respond(/roll\s*([^]+)/i, (msg) => {
-    let die = dieParser.parse(msg.match[1]);
-    msg.reply(report(die));
+    try {
+      let die = dieParser.parse(msg.match[1]);
+      msg.reply(report(die));
+    } catch (e) {
+      msg.reply(`:bomb: ${e.message}`);
+    }
   });
 };
