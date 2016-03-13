4775d3abe983493d90e5e41a9d9aa86521953d66
Ash Wilson <smashwilson@gmail.com>
2016-03-13 12:59:56 -0400

    Call the parse method.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const report = require('./ast').report;
 
 module.exports = (robot) => {
   robot.respond(/roll\s*([^]+)/i, (msg) => {
-    let die = dieParser(msg.match[1]);
+    let die = dieParser.parse(msg.match[1]);
     msg.reply(report(die));
   });
 };
