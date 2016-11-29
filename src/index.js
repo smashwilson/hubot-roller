e3373ff8e1a04bb274db4c1e0cede3ba42d9ff28
Ash Wilson <smashwilson@gmail.com>
2016-11-29 11:33:10 -0500

    Update the help text.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const report = require('./ast').report;
 
 module.exports = (robot) => {
   // Help
-  robot.commands.push('hubot roll <expression> - Roll some dice and report the total. Examples: d20, 4d6 + 3, (1d6)d(2d4).');
+  robot.commands.push('hubot roll <expression> - Roll some dice and report all intermediate rolls and the total. Examples: d20, 4d6 + 3, (1d6)d(2d4).');
+  robot.commands.push('hubot rollq <expression> - Roll some dice and report only the total.');
 
   robot.respond(/roll(q)?\s*([^]+)/i, (msg) => {
     let quiet = msg.match[1];
