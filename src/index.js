8e42e61065857c696dadcfb1a78b6a17dfe72fff
Ash Wilson <smashwilson@gmail.com>
2016-11-29 11:30:51 -0500

    Support !rollq.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ module.exports = (robot) => {
   // Help
   robot.commands.push('hubot roll <expression> - Roll some dice and report the total. Examples: d20, 4d6 + 3, (1d6)d(2d4).');
 
-  robot.respond(/roll\s*([^]+)/i, (msg) => {
+  robot.respond(/roll(q)?\s*([^]+)/i, (msg) => {
+    let quiet = msg.match[1];
+
     try {
-      let die = dieParser.parse(msg.match[1]);
-      msg.reply(report(die));
+      let die = dieParser.parse(msg.match[2]);
+      if (quiet === 'q') {
+        msg.send(die.value.toString());
+      } else {
+        msg.reply(report(die));
+      }
     } catch (e) {
       msg.reply(`:bomb: ${e.message}`);
     }
